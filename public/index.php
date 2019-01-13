aad64511ef16f4802605f3c39956370a2f2ff829
Panos <panagiotis.rapo@gmail.com>
2019-01-13 15:18:43 -0500

    T-T

--- public/index.php
+++ public/index.php
@@ -5,10 +5,10 @@
 <div id="main">
 
   <ul id="menu">
-    <li><a href="<?php echo url_for('/bicycles.php'); ?>">View Our Inventory</a></li>
+    <li><a href="<?php echo url_for('/bicycles.php'); ?>">View Our Inventories</a></li>
     <li><a href="<?php echo url_for('/about.php'); ?>">About Us</a></li>
   </ul>
-    
+
 </div>
 
 <?php $super_hero_image = 'AdobeStock_18040381_xlarge.jpeg'; ?>
